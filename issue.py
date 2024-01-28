bfc0418baaca00b6d45f9033128a88b313d91ba3
stevenlagoy <stevenlagoy@gmail.com>
2024-01-28 18:04:17 -0500

    added some context for some issues

---
+++ issue.py
@@ -0,0 +1,148 @@
+class Issue:
+    instances = []
+    def __init__(self, name, levels = []):
+        self.__class__.instances.append(self)
+        
+        self.name = name
+        self.levels = levels
+
+
+
+'''
+Issues in the US in 2020
+
+Economy
+    Social Security
+    Medicare
+    Tax Rates
+    Infrastructure
+    Investments
+    Debt
+    Retirement Age (make a continuum by year)
+        55
+        57
+        60
+        62
+        65 -- previous
+        67 -- current
+        70
+    Government Efficiency
+    Defense Spending
+        liquidation
+        0%
+        3%
+        6%
+        9%
+        12% of federal spending (3.5% GDP) 877 Billion -- current
+        15%
+    Inheritence Tax
+        0%
+        10%
+        20%
+        30%
+        40% -- current
+        50%
+Terrorism
+Foreign Policy
+    Threats
+    Allied Defense
+    NATO
+Health Care
+    Affordable Care Act
+
+Gun Policy
+        No limits
+        Restricted Military-grade weapons
+        Automatic weapons ban
+        Licencing for all firearms
+        Minimum age for licence
+        Very restricted gun ownership
+        Complete firearm ban
+    Restriction to those who would cause harm
+    Background Checks / Red-Flag Laws
+    Guns in Schools
+    Manufacturer Legal Immunity
+    Concealed Carry
+Crime
+    Marijuana Legalization
+        Legal
+        Illegal decriminalized
+        Illegal unenforced
+        Illegal
+        
+        Medical
+        Recreational
+Immigration
+    Refugees / Asylum
+    Undocumented Immigrants
+    Deportation
+    Family Separation
+    Visas
+    Job Availability
+    Allowing Criminals to enter / be tried in US
+    ICE Detention
+    Border Wall
+    Border Control (Military)
+    Birthright Citizenship
+    Counter-Cartel Military Operation
+    Ban from Muslim-Majority countries
+Education
+    Student Debt
+    Systemic Racism Education
+    Sexual Education
+    Transgender Students
+    Sports Integration
+    Gender-Affirming Care
+Treatment of Minorities
+Trade Policy
+    Aggressive Enforcement against "undercutting" markets
+    Intellectual property protection
+    Climate (carbon) sanctions
+    Trade union support
+    Reliance on national trading partners (globalism)
+    Sustainable growth
+    Tariffs
+    NAFTA
+    Trans-Pacific Partnership
+    WTO
+    Closing trade
+    Bailouts to farmers and businesses
+    Job outsourcing
+Environment
+    Paris Climate Agreement
+    Clean Air and Water protections
+    Climate Change
+        Belief in threat
+        Belief in human causes
+    Clean Energy
+    EV Subsidies
+    Market Force / Regulation
+        Rely on the market to solve problems
+        Rely on regulations to force solutions
+Abortion
+    Gestational limits (continuum)
+        6 weeks
+        12 weeks (before 2nd trimester)
+        15 weeks
+        18 weeks
+        20 weeks
+        22 weeks
+        24 weeks (fetal viability)
+        27 weeks (before 3rd trimester)
+    Risk to mother
+        risk to life
+        risk to health
+        fetal impairment
+        nonconsentual / incestual
+        socioeconomic factors
+    Legal standard for health/life
+        Reasonable medical judgement
+        Good faith clinical judgement
+        Reasonable cause
+    Illegality
+Treatment of LGBT+
+
+
+https://www.washingtonpost.com/politics/interactive/2023/presidential-candidates-2024-policies-issues/
+
+'''
