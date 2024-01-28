77dedaedd53e6742f9d3a7d9a0c92cd6c0192f4e
stevenlagoy <stevenlagoy@gmail.com>
2024-01-28 18:03:58 -0500

    created to test timer amounts (see "time" in workdoc)

---
+++ timer test.py
@@ -0,0 +1,17 @@
+# timer test
+import time as time
+from datetime import datetime
+from math import floor
+
+timecount = 0
+while timecount < 527040:
+    print(
+        str("January" if timecount < 44640 else "February" if timecount < 86400 else "") + " " + # month
+        str(floor(timecount / 1440) + 1) + " " + # day
+        str(floor(timecount / 60) % 24) + ":" + str(timecount - (60 * floor(timecount / 60))) # time
+    )
+    #print(str(floor(timecount / 60)) + ":" + str(timecount - (60 * floor(timecount / 60))))
+
+    #print(timecount)
+    timecount += 1
+    time.sleep(1/60)
