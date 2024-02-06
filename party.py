4d92eb74413a1bff76f66ef4938e631396fc94ae
stevenlagoy <stevenlagoy@gmail.com>
2024-02-06 01:52:19 -0500

    changed color codes, rewrote initialization function

--- party.py
+++ party.py
@@ -1,17 +1,18 @@
 class Party:
   instances = []
-  def __init__(self):
+  def __init__(self, name, adjective, abbreviation, governing_body, color_code, symbol, icon_address):
     self.__class__.instances.append(self)
-    self.longname = ""
-    self.shortname = ""
-    self.abbreviation = ""
-    self.demonym = ""
-    self.comittee = ""
-    self.alignment = [0,0]
-    self.members = []
-    self.chairperson = None
-    self.p_candidate = None
-    self.vp_candidate = None
+    self.name = name
+    self.adjective = adjective
+    self.abbreviation = abbreviation
+    self.governing_body = governing_body
+    self.color_code = color_code
+    self.symbol = symbol
+    self.icon_address = icon_address
+
+    self.members = [] # list of characters which are a part of the party
+    self.issues = [] # list of issues and the levels of support held by each party
+    self.conventions = [] # list of primaries and caucuses held by the party
     
 
 parties = [
@@ -20,7 +21,7 @@ parties = [
     "adjective" : "Democrats",
     "abbreviation" : "D",
     "governing body" : "Democratic National Committee",
-    "color" : "blue",
+    "color" : "#0044C9",
     "symbol" : "donkey",
     "icon" : "democrat_logo.png"
   },
@@ -29,7 +30,7 @@ parties = [
     "adjective" : "Republican",
     "abbreviation" : "R",
     "governing body" : "Republican National Committee",
-    "color" : "red",
+    "color" : "#E81B23",
     "symbol" : "elephant",
     "icon" : "republican_logo.png"
   },
@@ -38,7 +39,7 @@ parties = [
     "adjective" : "Bull Moose",
     "abbreviation" : "P",
     "governing body" : "",
-    "color" : "red",
+    "color" : "#B1333B",
     "symbol" : "bull moose",
     "icon" : "progressive_logo.png"
   },
@@ -47,7 +48,7 @@ parties = [
     "adjective" : "Democratic-Republican",
     "abbreviation" : "D-R",
     "governing body" : "",
-    "color" : "red, white, blue",
+    "color" : "#FFFDFD",
     "symbol" : "",
     "icon" : "democratic_republican_cockade.png"
   },
@@ -56,7 +57,7 @@ parties = [
     "adjective" : "Federalist",
     "abbreviation" : "F",
     "governing body" : "",
-    "color" : "black, white",
+    "color" : "#000000",
     "symbol" : "",
     "icon" : "federalist_cockade.png"
   },
@@ -65,7 +66,7 @@ parties = [
     "adjective" : "Green",
     "abbreviation" : "G",
     "governing body" : "Green National Committee",
-    "color" : "green",
+    "color" : "#00A95C",
     "symbol" : "Sunflower",
     "icon" : "green_logo.png"
   },
@@ -74,7 +75,7 @@ parties = [
     "adjective" : "Liberatarian",
     "abbreviation" : "L",
     "governing body" : "Liberatarian National Committee",
-    "color" : "yellow",
+    "color" : "#FED105",
     "symbol" : "hedgehog", # the official symbol is the Statue of Liberty - the hedgehog is used unofficially by many party members
     "icon" : "democratic_republican_cockade.png"
   },
@@ -83,7 +84,7 @@ parties = [
     "adjective" : "Whig",
     "abbreviation" : "W",
     "governing body" : "",
-    "color" : "blue, buff",
+    "color" : "#044E77",
     "symbol" : "owl", # the owl is only used contemporarily by the Modern Whig Institute
     "icon" : "whig_logo.png"
   },
@@ -92,17 +93,17 @@ parties = [
     "adjective" : "Populist",
     "abbreviation" : "P",
     "governing body" : "",
-    "color" : "red",
+    "color" : "#FF0000",
     "symbol" : "",
     "icon" : "populist_logo.png"
   },
   {
-    "name" : "Socialist Party",
+    "name" : "Socialist Democratic Party", # there are several real parties, including: Social Democrats, USA; Socialist Party USA; and the Democratic Socialist Organizing Committee
     "adjective" : "Socialist",
     "abbreviation" : "S",
     "governing body" : "",
-    "color" : "red",
-    "symbol" : "",
+    "color" : "#ED1F24",
+    "symbol" : "Fist with Rose",
     "icon" : "socialist_logo.png"
   },
   {
@@ -110,8 +111,19 @@ parties = [
     "adjective" : "Centrist",
     "abbreviation" : "", # no abbreviation
     "governing body" : "", # no governing body
-    "color" : "gray",
+    "color" : "#3F3F3F",
     "symbol" : "balances",
     "icon" : "centrist_icon.png"
   }
 ]
+
+for party in parties:
+  Party(
+    party.get("name"),
+    party.get("adjective"),
+    party.get("abbreviation"),
+    party.get("governing body"),
+    party.get("color"),
+    party.get("symbol"),
+    party.get("icon")
+  )
