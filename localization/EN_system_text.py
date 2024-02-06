cf7efe87ebdefb4225211e5aa61b84755ca79ce8
stevenlagoy <stevenlagoy@gmail.com>
2024-02-06 11:06:47 -0500

    created system text localization

---
+++ localization/EN_system_text.py
@@ -0,0 +1,7 @@
+EN_system_text { # list of English text which will be displayed by the game for titles and labels etc
+  "title" : "Race for the Presidency",
+  "newsave_text" : "New Game",
+  "opensave_text" : "Continue Game",
+  "settings_text" : "Settings",
+  "open_game_text" : "Open Save"
+}
