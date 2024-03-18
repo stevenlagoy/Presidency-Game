0db61d970ffaf2c71b12e3637008d9bcc0009239
stevenlagoy <stevenlagoy@gmail.com>
2024-03-18 00:16:46 -0400

    create percentage_roll function

--- engine.py
+++ engine.py
@@ -1,4 +1,5 @@
 from datetime import datetime
+import random as rand
 
 def error_log(logline: str) -> None:
     '''Writes the specified logline into "log.txt". Pass a string containing information about the error and the place it was created.'''
@@ -10,3 +11,11 @@ def error_log(logline: str) -> None:
 def get_timestamp() -> str:
     ''' Returns the current date and time as a string. To be used for logging applications. '''
     return datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+
+def percentage_roll(chance: int, affirmValue: bool = True) -> bool:
+    ''' Returns boolean randomly based on the passed percentage chance. An affirmative roll will return affirmValue. '''
+
+    if chance >= 1: return affirmValue # if the chance is 100% or greater, always True
+    elif chance <= 0: return not affirmValue # if the chance is 0% or less, always False
+    else:
+        return affirmValue if rand.uniform(0, 1) <= chance else not affirmValue
