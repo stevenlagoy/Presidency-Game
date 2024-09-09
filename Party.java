b7ba7feb0df5a307a3d81917b27463569f248f42
stevenlagoy <stevenlagoy@gmail.com>
2024-09-09 00:07:04 -0400

    Add getters and setters

--- Party.java
+++ Party.java
@@ -2,6 +2,8 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public class Party
 {
@@ -16,8 +18,8 @@ public class Party
     //private ??? symbol like the animal that represents the party
     private File iconFile;
 
-    private List<Character> members = new ArrayList<>();
-    private List<Convention> conventions = new ArrayList<>();
+    private Set<Character> members = new HashSet<>();
+    private Set<Convention> conventions = new HashSet<>();
     // list for issues and stances as well
 
     public Party(String buildstring)
@@ -36,6 +38,66 @@ public class Party
             throw(new Exception());
         }
     }
+
+    public String getName(){
+        return this.name;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public String getAdjective(){
+        return this.adjective;
+    }
+    public void setAdjective(String adjective){
+        this.adjective = adjective;
+    }
+    public String getAbbr(){
+        return this.abbreviation;
+    }
+    public void setAbbr(String abbreviation){
+        this.abbreviation = abbreviation;
+    }
+    public int getColorCode(){
+        return this.colorCode;
+    }
+    public void setColorCode(int colorCode){
+        this.colorCode = colorCode;
+    }
+    public File getIconFile(){
+        return this.iconFile;
+    }
+    public void setIconFile(String iconFilePath) throws Exception{
+        this.iconFile = new File(iconFilePath);
+        if(!iconFile.exists()){
+            throw(new Exception());
+        }
+    }
+    public Set<Character> getMembers(){
+        return this.members;
+    }
+    public void addMember(Character c){
+        this.members.add(c);
+    }
+    public void removeMember(Character c){
+        this.members.remove(c);
+    }
+    public void resetMembers(){
+        this.members.clear();
+    }
+    public Set<Convention> getConventions(){
+        return this.conventions;
+    }
+    public void addConvention(Convention c){
+        this.conventions.add(c);
+    }
+    public void removeConvention(Convention c){
+        this.conventions.remove(c);
+    }
+    public void resetConventions(){
+        this.conventions.clear();
+    }
+
+
 
     public String toString(){
         return "";
