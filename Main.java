5a7721fb116aa1b84f19ca167e47e52449f7c63b
stevenlagoy <stevenlagoy@gmail.com>
2024-09-25 17:07:58 -0400

    create getInput functions

--- Main.java
+++ Main.java
@@ -2,29 +2,7 @@ import java.util.Scanner;
 
 public class Main{
 
-    public static void main(String[] args){
-
-        System.out.println("Race for the Presidency\n[NEW / CONTINUE]");
-        
-        while(!goodIn){
-            System.out.print("> ");
-            input = in.nextLine();
-            switch(input){
-                case "NEW":
-                case "N":
-                    goodIn = true;
-                    break;
-                case "CONTINUE":
-                case "CONT":
-                case "C":
-                    goodIn = false;
-                    break;
-            }
-        }
-        System.out.println("Start game");
-    }
-
-    public int getInput(String[] responses){
+    public static int getInput(String[] responses){
         /* getInput
          * takes responses, a list of acceptable strings
          * only returns when an acceptable input is given
@@ -34,8 +12,34 @@ public class Main{
         Scanner in = new Scanner(System.in);
         String input = "";
         while(true){
+            System.out.print("> ");
             input = in.nextLine();
-            responses.;
+            for(int i = 0; i < responses.length; i++){
+                if(responses[i].equals(input)) return i;
+            }
         }
+    }
+    public static int getInput(){
+        /* getInput
+         * asks for an input until one is given
+         * returns when input is recieved
+        */
+
+        Scanner in = new Scanner(System.in);
+        String input = "";
+        while(true){
+            System.out.print("> ");
+            input = in.nextLine();
+            if(input != null) return 1;
+        }
+    }
+
+    public static void main(String[] args){
+
+        System.out.println("Race for the Presidency\n[NEW / CONTINUE]");
+        
+        System.out.println("Press enter to start");
+        getInput();
+        System.out.println("Start game");
     }
 }
