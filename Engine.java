fded60574b80a87f9d03f27d8a4510834a435dc1
stevenlagoy <stevenlagoy@gmail.com>
2024-10-09 23:41:49 -0400

    make engine a static class

--- Engine.java
+++ Engine.java
@@ -7,30 +7,31 @@ import java.util.Scanner;
 import java.io.IOException;
 
 public class Engine {
-    public int getInput(String[] responses){
+    static Scanner in = new Scanner(System.in);
+    public static int getInput(String[] responses){
         /* getInput
          * takes responses, a list of acceptable strings
          * only returns when an acceptable input is given
          * returns an integer corresponding to the index of the accepted string in the responses list
         */
 
-        Scanner in = new Scanner(System.in);
         String input = "";
         while(true){
             System.out.print("> ");
             input = in.nextLine().toUpperCase();
             for(int i = 0; i < responses.length; i++){
-                if(responses[i].equals(input)) return i;
+                if(responses[i].equals(input)){
+                    return i;
+                }
             }
         }
     }
-    public int getInput(){
+    public static int getInput(){
         /* getInput
          * asks for an input until one is given
          * returns when input is recieved
         */
 
-        Scanner in = new Scanner(System.in);
         String input = "";
         while(true){
             System.out.print("> ");
@@ -38,7 +39,7 @@ public class Engine {
             if(input != null) return 1;
         }
     }
-    public void log(String logline){
+    public static void log(String logline){
         try {
             File logFile = new File("log.txt");
             logFile.createNewFile(); // does nothing if already exists
@@ -53,7 +54,7 @@ public class Engine {
             System.exit(-1);
         }
     }
-    public void log(String context, String logline){
+    public static void log(String context, String logline){
         try {
             File logFile = new File("log.txt");
             logFile.createNewFile(); // does nothing if already exists
@@ -68,7 +69,7 @@ public class Engine {
             System.exit(-1);
         }
     }
-    public void log(String context, String logline, String trace){
+    public static void log(String context, String logline, String trace){
         try {
             File logFile = new File("log.txt");
             logFile.createNewFile(); // does nothing if already exists
@@ -83,7 +84,7 @@ public class Engine {
             System.exit(-1);
         }
     }
-    public void log(Exception logE){
+    public static void log(Exception logE){
         try {
             File logFile = new File("log.txt");
             logFile.createNewFile(); // does nothing if already exists
@@ -99,11 +100,11 @@ public class Engine {
         }
     }
 
-    public String getDate(){
+    public static String getDate(){
         return new SimpleDateFormat("yyyy.MM.dd.HH.mm.ss.SSS").format(new java.util.Date());
     }
 
-    public void reset() throws IOException {
+    public static void reset() throws IOException {
         File logFile = new File("log.txt");
         logFile.createNewFile(); // does nothing if already exists
         FileOutputStream logStream = new FileOutputStream(logFile, false);
