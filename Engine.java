f4e85142b4c11ca4c56e33b82fbd2b742b8af875
stevenlagoy <stevenlagoy@gmail.com>
2024-11-25 22:22:54 -0500

    Add probabalistic selection functions

--- Engine.java
+++ Engine.java
@@ -3,6 +3,7 @@ import java.io.FileOutputStream;
 import java.io.FileWriter;
 import java.io.PrintWriter;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Random;
 import java.util.Scanner;
 import java.io.IOException;
@@ -114,28 +115,141 @@ public class Engine {
         return;
     }
 
+    /**
+     * Selects a float between 0.0 and 1.0 which can be used as a percentage.
+     * 
+     * @return A pseudorandomly selected float in the range [0.0, 1.0)
+     */
     public static float randPercent(){
         return randPercent(0.0f, 1.0f);
     }
-
+    /**
+     * Selects a float between the min and the max.
+     * 
+     * @param min The minimum value which can be selected (inclusive)
+     * @param max The maximum value which can be selected (exclusive)
+     * @return A pseudorandomly selected float in the range [min, max)
+     */
     public static float randPercent(float min, float max){
         // will perform the same if min and max are flipped
         Random rand = new Random();
-        return (min - max) * rand.nextFloat() + min; // return a float between min and max (exclusive), equally distributed
+        return (min - max) * rand.nextFloat() + min; // return a float between min and max (exclusive), evenly distributed
     }
+
+    /**
+     * Selects a double between 0.0 and the max.
+     * 
+     * @param max The maximum value which can be selected (exclusive)
+     * @return A pseudorandomly selected double in the range [0.0, max)
+     */
+    public static double randDouble(double max){
+        return randDouble(0.0, max);
+    }
+    /**
+     * Selects a double between the min and the max.
+     * 
+     * @param min The minimum value which can be selected (inclusive)
+     * @param max The maximum value which can be selected (exclusive)
+     * @return A pseudorandomly selected double in the range [min, max)
+     */
     public static double randDouble(double min, double max){
         // will perform the same if min and max are flipped
         Random rand = new Random();
-        return (min - max) * rand.nextDouble() + min; // return a double between min and max (exclusive), equally distributed
+        return (max - min) * rand.nextDouble() + min; // return a double between min and max (exclusive), evenly distributed
     }
-
+    
+    /**
+     * Selects an integer between zero and the max, evenly distributed.
+     * 
+     * @param max The maximum value which can be randomly selected (exclusive)
+     * @return A pseudorandomly generated integer in the range [0, max)
+     */
     public static int randInt(int max){
         return randInt(0, max);
     }
-
+    /**
+     * Selects an integer between the min and the max, evenly distributed.
+     * 
+     * @param min The minimum value which can be selected (inclusive)
+     * @param max The maximum value which can be selected (exclusive)
+     * @return A pseudorandomly generated integer in the range [min, max)
+     */
     public static int randInt(int min, int max){
-        if(max > min) throw new IllegalArgumentException(String.format("The minimum is less than the maximum: %d < %d.%n", max, min));
+        if(max < min) throw new IllegalArgumentException(String.format("The minimum is less than the maximum: %d < %d.%n", max, min));
         Random rand = new Random();
-        return rand.nextInt(max - min + 1) + min; // return an integer between min and max (inclusive), equally distributed
+        return rand.nextInt(max - min) + min; // return an integer between min and max (exclusive), evenly distributed
     }
+    
+    /** 
+     * Selects an integer based on a list of weights.
+     * Probabilistically selects an index based on the values in the passed array, and returns the index.
+     * No value in the weights array may be negative (<0). At least one value must be greater than zero.
+     * 
+     * @param weights Array of weights
+     * @return Probabalistically selected index
+     */
+    public static int weightedInt(int[] weights) {
+        int totalWeight = 0;
+        int[] cumulativeWeights = new int[weights.length];
+        for (int i = 0; i < weights.length; i++) {
+            if (weights[i] < 0) 
+                throw new IllegalArgumentException(String.format("Negative value not allowed as probability weight: %d < 0.", weights[i]));
+            totalWeight += weights[i];
+            cumulativeWeights[i] = totalWeight;
+        }
+        if (totalWeight == 0) {
+            throw new IllegalArgumentException("At least one weight must be greater than zero.");
+        }
+        int res = randInt(totalWeight); // Generates [0, totalWeight)
+        int low = 0, high = weights.length - 1, mid;
+        while (low < high) {
+            mid = (low + high) / 2;
+            if (res < cumulativeWeights[mid])
+                high = mid;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+    /**
+     * Selects a double based on a list of weights.
+     * Probabilistically selects an index based on the values in the passed array, and returns the index.
+     * No value in the weights array may be negative (<0). At least one value must be greater than zero.
+     * 
+     * @param weights Array of weights
+     * @return Probabalistically selected index
+     */
+    public static int weightedDouble(double[] weights) {
+        double totalWeight = 0.0;
+        double[] cumulativeWeights = new double[weights.length];
+        
+        for (int i = 0; i < weights.length; i++) {
+            if (weights[i] < 0) {
+                throw new IllegalArgumentException(String.format("Negative value not allowed as probability weight: %f < 0.", weights[i]));
+            }
+            totalWeight += weights[i];
+            cumulativeWeights[i] = totalWeight;
+        }
+    
+        if (totalWeight == 0.0) {
+            throw new IllegalArgumentException("At least one weight must be greater than zero.");
+        }
+    
+        // Generate a random double in the range [0.0, totalWeight)
+        double res = randDouble(totalWeight);
+    
+        // Binary search for the appropriate index
+        int low = 0, high = weights.length - 1, mid;
+        while (low < high) {
+            mid = (low + high) / 2;
+            if (res < cumulativeWeights[mid]) {
+                high = mid;
+            } else {
+                low = mid + 1;
+            }
+        }
+    
+        return low;
+    }
+    
 }
--- Main.java
+++ Main.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+
 // import java.util.Scanner;
 import javafx.application.Application;
 // import javafx.application.Platform;
@@ -11,7 +12,11 @@ public class Main
 {
     public static void main(String[] args) throws IOException {
         Engine.reset();
+        
+        int[] weights = {1, 1, 1};
 
-        Application.launch(MapApplication.class, args);
+        Engine.weightedInt(weights);
+
+        //Application.launch(MapApplication.class, args);
     }
 }
