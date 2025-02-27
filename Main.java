9a585c6e88649ae3f383a27f37bb87307f0e0c23
stevenlagoy <stevenlagoy@gmail.com>
2025-02-27 02:16:47 -0500

    Add localization support to Engine

--- Main.java
+++ Main.java
@@ -1,46 +1,25 @@
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.Date;
-import java.util.HashMap;
-
 public class Main
 {
-    public static void errorFunc(){
-        errorFunc2();
-    }
-    public static void errorFunc2(){
-        throw new UnsupportedOperationException("Test");
-    }
     public static void main(String[] args){
         Engine.init();
+        Engine.language = Engine.Language.EN;
         boolean active = true;
+        
+        // Initialize and run OpenGL window
+        Engine.initOpenGL();
+        Engine.runOpenGL();
 
-        //HashMap<Object, Object> json = Engine.readJSONFile("blocs.json");
-        //System.out.println(json.toString());
-
-        Engine.log("TEST LOG", "This is a test of the log system.", new Exception());
-
-        System.out.println(Bloc.getInstances().toString());
-
-        for(int i = 0; i < 10000; i++){
-            Character character = new Character();
-        }
-
-        /*
-        while(active){
+        while (active) {
             try {
                 active = Engine.tick();
-                if(!active) break;
+                if (!active) break;
                 Thread.sleep(Engine.tickSpeed);
-            } catch (InterruptedException e) {
+            }
+            catch (InterruptedException e) {
                 e.printStackTrace();
                 break;
             }
         }
-        */
 
         System.out.print("Main Done\n");
         Engine.done();
