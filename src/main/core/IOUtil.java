8f1b24256d5d5002cd4749cb098d3589fbc0cc54
stevenlagoy <stevenlagoy@gmail.com>
2025-06-25 00:29:21 -0400

    Remove old unused imports

--- src/main/core/IOUtil.java
+++ src/main/core/IOUtil.java
@@ -11,16 +11,12 @@ package main.core;
 
 // IMPORTS ----------------------------------------------------------------------------------------
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.Closeable;
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
