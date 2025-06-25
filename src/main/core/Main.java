d67de753919477914a046d73e2d1228612cb085f
stevenlagoy <stevenlagoy@gmail.com>
2025-06-25 00:29:41 -0400

    Simplify entry point logic

--- src/main/core/Main.java
+++ src/main/core/Main.java
@@ -1,47 +1,27 @@
 /*
  * Main.java
  * Steven LaGoy
- * 
  */
 
 package main.core;
 
-import main.core.characters.names.NameManager;
-import main.core.demographics.DemographicsManager;
+public class Main {
 
-public class Main
-{
+    /** This class is non-instantiable. The {@code main()} entry point should be accessed in a static way. */
+    private Main() {} // Non-Instantiable
 
-    public static void main(String[] args){
-        boolean active = true;
+    public static void main(String[] args) {
+        int errorCode = 0;
         try {
-            // Engine.language = Engine.Language.EN;
-            active = active && Engine.init();
-            //if (active) Engine.run();
+            if (!Engine.start()) return;
         }
         catch (Exception e) {
+            errorCode = e.hashCode();
             e.printStackTrace();
-            active = false;
         }
-
-        for (int i = 0; i >= 0; i++) {
-            IOUtil.stdout.println(NameManager.generateName(DemographicsManager.randomDemographics()).getBiographicalName());
+        finally {
+            Engine.log("Main Done");
+            System.exit(errorCode);
         }
-
-        while (active) {
-            try {
-                active = Engine.tick();
-                if (!active) break;
-                Thread.sleep(Engine.getTickSpeed());
-            }
-            catch (InterruptedException e) {
-                e.printStackTrace();
-                active = false;
-            }
-        }
-        Engine.writeSave();
-        Engine.stop();
-        System.out.print("Main Done\n");
-        System.exit(0);
     }
 }
